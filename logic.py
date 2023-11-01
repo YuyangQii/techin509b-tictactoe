10a374ee3ba80c8e6ed3abb006e3550259fcb5dd
Weexgnpg <972563648@qq.com>
2023-11-01 10:47:07 +0800

    fix the code

--- logic.py
+++ logic.py
@@ -14,9 +14,21 @@ def make_empty_board():
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] is not None:
+            return row[0]
+    
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] is not None:
+            return board[0][col]
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not None:
+        return board[0][0]
+    
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not None:
+        return board[0][2]
     return None  # FIXME
 
 
 def other_player(player):
     """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+    return "O" if player == 'X' else 'X' # FIXME
